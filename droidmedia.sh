306dae48fc53764eabba642d57b833efbf07f982
Alin M ELena <alin-marin.elena@stfc.ac.uk>
2015-07-26 15:23:02 +0100

    droidmedia stuff

--- droidmedia.sh
+++ droidmedia.sh
@@ -9,10 +9,10 @@
 
 source ~/.hadk.env
 
-cp pack-source.sh $ANDROID_ROOT/
+cp pack-droidmedia.sh $ANDROID_ROOT/
 cp droidmedia.spec $ANDROID_ROOT/external/droidmedia/rpm/
 cd $ANDROID_ROOT
-sh pack-source.sh
+sh pack-droidmedia.sh
 
     PKG=droidmedia
     echo -e "\e[01;32m Info: build $PKG\e[00m"
@@ -26,3 +26,6 @@ sh pack-source.sh
 
     echo -e "\e[01;32m Info: mb2 -s rpm/$PKG.spec -t $VENDOR-$DEVICE-armv7hl build &> $PKG.log \e[00m"
     mb2 -s rpm/$PKG.spec -t $VENDOR-$DEVICE-armv7hl build #&> $ANDROID_ROOT/$PKG.log 
+    cd $ANDROID_ROOT
+    mkdir -p droid-local-repo/$DEVICE
+    cp $MER_ROOT/devel/droidmedia/RPMS/*.rpm droid-local-repo/$DEVICE/
